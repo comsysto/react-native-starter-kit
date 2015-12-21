60da9053667e492923a5e485e0af7fe1a66857d3
Thomas Sojka <thomas@Thomass-MacBook-Pro.local>
2015-12-21 13:56:59 +0100

    Create App.js as start point

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict'
+import React, { AppRegistry } from 'react-native'
+import App from './src/containers/App'
 
-var React = require('react-native')
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} = React
-
-var ReactNativeStarterKit = React.createClass({
-  render: function () {
+class Main extends React.Component {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <App />
     )
   }
-})
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
-
-AppRegistry.registerComponent('ReactNativeStarterKit', () => ReactNativeStarterKit)
+AppRegistry.registerComponent('ReactNativeStarterKit', () => Main)
